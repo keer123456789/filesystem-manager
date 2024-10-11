abd7d6e41237f6e449eedc30e846c760ea01feb0
zhangjinglun <15110074528@163.com>
2024-10-11 17:50:39 +0800

    增加文件系统操作器和权限管理器

---
+++ src/main/java/com/keer/fsm/FileAuthorityOperator.java
@@ -0,0 +1,9 @@
+package com.keer.fsm;
+
+/**
+ * @Author: 张经伦
+ * @Date: 2024/10/11  9:13
+ * @Description:
+ */
+public interface FileAuthorityOperator {
+}
---
+++ src/main/java/com/keer/fsm/FileOperator.java
@@ -0,0 +1,10 @@
+package com.keer.fsm;
+
+/**
+ * @Author: 张经伦
+ * @Date: 2024/10/11  9:08
+ * @Description: 文件权限操作器
+ */
+public interface FileOperator {
+
+}
